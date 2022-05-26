51f26c9d59c629787d8b5eb48e9aef9a3765e012
luncheon <takeuck+github@gmail.com>
2022-05-26 17:18:17 +0900

    change compile target to esnext

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
 "use strict";
-addEventListener('pointerdown', function (pointerDownEvent) {
-    var _a;
-    var separator = pointerDownEvent.target;
-    var container = separator.parentElement;
+addEventListener('pointerdown', (pointerDownEvent) => {
+    const separator = pointerDownEvent.target;
+    const container = separator.parentElement;
     if (!container || !pointerDownEvent.isPrimary || pointerDownEvent.button !== 0 || separator.getAttribute('role') !== 'separator') {
         return;
     }
-    var vertical = container.hasAttribute('data-flex-splitter-vertical');
-    var horizontal = container.hasAttribute('data-flex-splitter-horizontal');
+    const vertical = container.hasAttribute('data-flex-splitter-vertical');
+    const horizontal = container.hasAttribute('data-flex-splitter-horizontal');
     if (!vertical && !horizontal) {
         return;
     }
     // prevent text selection
     pointerDownEvent.preventDefault();
-    var pointerId = pointerDownEvent.pointerId;
-    var pane1 = separator.previousElementSibling;
-    var pane2 = separator.nextElementSibling;
-    var containerStyle = getComputedStyle(container);
+    const pointerId = pointerDownEvent.pointerId;
+    let pane1 = separator.previousElementSibling;
+    let pane2 = separator.nextElementSibling;
+    const containerStyle = getComputedStyle(container);
     if ((containerStyle.flexDirection.indexOf('reverse') !== -1 ? -1 : 1) * (horizontal && containerStyle.direction === 'rtl' ? -1 : 1) === -1) {
-        _a = [pane2, pane1], pane1 = _a[0], pane2 = _a[1];
+        [pane1, pane2] = [pane2, pane1];
     }
-    var pane1ComputedStyle = getComputedStyle(pane1);
-    var pane2ComputedStyle = getComputedStyle(pane2);
-    var pane1Rect = pane1.getBoundingClientRect();
-    var onPointerMove;
+    const pane1ComputedStyle = getComputedStyle(pane1);
+    const pane2ComputedStyle = getComputedStyle(pane2);
+    const pane1Rect = pane1.getBoundingClientRect();
+    let onPointerMove;
     if (vertical) {
-        var pane1Pos_1 = pane1Rect.top + pointerDownEvent.offsetY;
-        var totalSize_1 = pane1.offsetHeight + pane2.offsetHeight;
-        var pane1MinSize_1 = Math.max(parseInt(pane1ComputedStyle.minHeight, 10) || 0, totalSize_1 - (parseInt(pane2ComputedStyle.maxHeight, 10) || totalSize_1));
-        var pane1MaxSize_1 = Math.min(parseInt(pane1ComputedStyle.maxHeight, 10) || totalSize_1, totalSize_1 - (parseInt(pane2ComputedStyle.minHeight, 10) || 0));
-        onPointerMove = function (event) {
+        const pane1Pos = pane1Rect.top + pointerDownEvent.offsetY;
+        const totalSize = pane1.offsetHeight + pane2.offsetHeight;
+        const pane1MinSize = Math.max(parseInt(pane1ComputedStyle.minHeight, 10) || 0, totalSize - (parseInt(pane2ComputedStyle.maxHeight, 10) || totalSize));
+        const pane1MaxSize = Math.min(parseInt(pane1ComputedStyle.maxHeight, 10) || totalSize, totalSize - (parseInt(pane2ComputedStyle.minHeight, 10) || 0));
+        onPointerMove = event => {
             if (event.pointerId === pointerId) {
-                var pane1Size = Math.round(Math.min(Math.max(event.clientY - pane1Pos_1, pane1MinSize_1), pane1MaxSize_1));
+                const pane1Size = Math.round(Math.min(Math.max(event.clientY - pane1Pos, pane1MinSize), pane1MaxSize));
                 pane1.style.height = pane1Size + 'px';
-                pane2.style.height = totalSize_1 - pane1Size + 'px';
+                pane2.style.height = totalSize - pane1Size + 'px';
             }
         };
     }
     else {
-        var pane1Pos_2 = pane1Rect.left + pointerDownEvent.offsetX;
-        var totalSize_2 = pane1.offsetWidth + pane2.clientWidth;
-        var pane1MinSize_2 = Math.max(parseInt(pane1ComputedStyle.minWidth, 10) || 0, totalSize_2 - (parseInt(pane2ComputedStyle.maxWidth, 10) || totalSize_2));
-        var pane1MaxSize_2 = Math.min(parseInt(pane1ComputedStyle.maxWidth, 10) || totalSize_2, totalSize_2 - (parseInt(pane2ComputedStyle.minWidth, 10) || 0));
-        onPointerMove = function (event) {
+        const pane1Pos = pane1Rect.left + pointerDownEvent.offsetX;
+        const totalSize = pane1.offsetWidth + pane2.clientWidth;
+        const pane1MinSize = Math.max(parseInt(pane1ComputedStyle.minWidth, 10) || 0, totalSize - (parseInt(pane2ComputedStyle.maxWidth, 10) || totalSize));
+        const pane1MaxSize = Math.min(parseInt(pane1ComputedStyle.maxWidth, 10) || totalSize, totalSize - (parseInt(pane2ComputedStyle.minWidth, 10) || 0));
+        onPointerMove = event => {
             if (event.pointerId === pointerId) {
-                var pane1Size = Math.round(Math.min(Math.max(event.clientX - pane1Pos_2, pane1MinSize_2), pane1MaxSize_2));
+                const pane1Size = Math.round(Math.min(Math.max(event.clientX - pane1Pos, pane1MinSize), pane1MaxSize));
                 pane1.style.width = pane1Size + 'px';
-                pane2.style.width = totalSize_2 - pane1Size + 'px';
+                pane2.style.width = totalSize - pane1Size + 'px';
             }
         };
     }
-    var onPointerUp = function (event) {
+    const onPointerUp = (event) => {
         if (event.pointerId === pointerId) {
-            // separator.releasePointerCapture(pointerId)
             separator.removeEventListener('pointermove', onPointerMove);
             separator.removeEventListener('pointerup', onPointerUp);
             separator.removeEventListener('pointercancel', onPointerUp);
